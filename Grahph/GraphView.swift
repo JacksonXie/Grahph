a9dbcf6c1494b529dd63b898252e3e75e4a0f681
JacksonXie <minjiexie@ua-unistudent-ten-9-194-33.uniaccess.unimelb.edu.au>
2015-01-29 13:04:22 +1100

    created

---
+++ Grahph/GraphView.swift
@@ -0,0 +1,16 @@
+//
+//  GraphView.swift
+//  Grahph
+//
+//  Created by Minjiexie on 15/1/29.
+//  Copyright (c) 2015年 MinjieXie. All rights reserved.
+//
+
+import UIKit
+
+class RegisterTopView: UIView {
+    
+    override func drawRect(rect: CGRect) {
+    }
+}
+
